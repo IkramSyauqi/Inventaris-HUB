b04a0203d5aded4c43985da8a36635c970dbfa5d
Ikram Syauqi Kusej <ikramskei@gmail.com>
2024-10-30 13:31:12 +0700

    Update setupProxy.js fix

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,29 +1,11 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function (app) {
+module.exports = function(app) {
   app.use(
-    ['users/login','products/', 'users/logout', '/users/get/all', '/users/:id'],  
+    ['users/login','products/', 'users/logout', 'users/get/all', 'users/:id'],  // Ubah ini ke path yang sesuai dengan endpoint API Anda
     createProxyMiddleware({
       target: 'https://inventaris-app-backend.vercel.app', 
       changeOrigin: true,
-      secure: false,
-      logLevel: 'debug',
-
-      // onProxyReq: (proxyReq, req, res) => {
-      //   // Log detail request
-      //   console.log('Request URL:', req.url);
-      //   console.log('Request Method:', req.method);
-      //   console.log('Request Headers:', req.headers);
-      // },
-      // onProxyRes: (proxyRes, req, res) => {
-      //   // Log response
-      //   console.log('Response Status:', proxyRes.statusCode);
-      //   console.log('Response Headers:', proxyRes.headers);
-      // },
-      // onError: (err, req, res) => {
-      //   console.error('Proxy Error:', err);
-      //   res.status(500).send('Proxy Error');
-      // },
     })
   );
 };
