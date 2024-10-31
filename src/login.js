4a6e43ebbc30a183fade4bfcfe96dc02ead57b95
Ikram Syauqi Kusej <ikramskei@gmail.com>
2024-10-31 10:50:00 +0700

    Update login.js loading screen

--- src/login.js
+++ src/login.js
@@ -8,9 +8,11 @@ const Login = () => {
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
   const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setIsLoading(true);
 
     try {
       // Akses API melalui proxy
@@ -26,6 +28,8 @@ const Login = () => {
       }
     } catch (error) {
       setError('Login gagal, periksa username dan password Anda.');
+    } finally {
+      setIsLoading(false); 
     }
   };
 
@@ -65,7 +69,34 @@ const Login = () => {
                   />
                 </div>
                 {error && <div className="alert alert-danger">{error}</div>}
-                <button type="submit" className="button button-outline w-100">Login</button>
+                <button
+                  type="submit"
+                  className="button button-outline w-100"
+                  disabled={isLoading}
+                >
+                  {isLoading ? (
+                    <div className="flex items-center justify-center">
+                      <svg className="animate-spin h-5 w-5 mr-2 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                        <circle
+                          className="opacity-25"
+                          cx="12"
+                          cy="12"
+                          r="10"
+                          stroke="currentColor"
+                          strokeWidth="4"
+                        ></circle>
+                        <path
+                          className="opacity-75"
+                          fill="currentColor"
+                          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291l1.383-1.921a6 6 0 01-1.383-3.37H2a8 8 0 004 7.291z"
+                        ></path>
+                      </svg>
+                      Loading...
+                    </div>
+                  ) : (
+                    'Login'
+                  )}
+                </button>
               </form>
             </div>
           </div>
